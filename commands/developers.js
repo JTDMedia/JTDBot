d68b7fc955d7c0040556e445e3f1cea28b69f233
JTDMedia <oudegraftak@proton.me>
2024-11-25 21:40:04 +0100

    Update developers.js

--- commands/developers.js
+++ commands/developers.js
@@ -18,7 +18,7 @@ module.exports = {
 				.setDescription('Run a piece of code and get the result'))
     			.addStringOption(option =>
 		option.setName('code')
-			.setDescription('The code to eval'));
+			.setDescription('The code to eval'))
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('fetch')
